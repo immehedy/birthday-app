889eba5b0db69b94b0bded66e49dd58d43a74c4e
mehedy hassan <mehedy.hassan@northsouth.edu>
2022-01-20 00:53:31 +0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 1,
+    name: 'Bertie Yates',
+    age: 29,
+    date: '19-01-1993',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Hester Hogan',
+    age: 32,
+    date: '19-01-1993',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Larry Little',
+    age: 36,
+    date: '19-01-1993',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sean Walsh',
+    age: 34,
+    date: '19-01-1993',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Lola Gardner',
+    age: 27,
+    date: '19-02-1993',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
